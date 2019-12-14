023eaa3c05167005df6f2517dd96487fb680e6b7
Denizcan <denizcandemir@outlook.com>
2019-12-14 22:32:25 +0300

    code refactoring

--- client/src/component/Menu.js
+++ client/src/component/Menu.js
@@ -1,45 +1,36 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import ListSubheader from '@material-ui/core/ListSubheader';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
 
- 
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
-export default function Menu(){
+export default function FolderList() {
+  const classes = useStyles();
 
-    const [selectedIndex, setSelectedIndex] = React.useState(1);
-  
-    const handleListItemClick = (event, index) => {
-      setSelectedIndex(index);
-    };
-
-    return(
-        <div>
-            <List component="nav" aria-label="main mailbox folders" >
-            <ListSubheader component="div" id="nested-list-subheader">
-                Groups
-            </ListSubheader>
-                <ListItem button selected={selectedIndex === 0} onClick={event => handleListItemClick(event, 0)} >
-                <ListItemIcon>
-                    <InboxIcon />
-                </ListItemIcon>
-                <ListItemText primary="Group1" />
-                </ListItem>
-                <ListItem button selected={selectedIndex === 1} onClick={event => handleListItemClick(event, 1)} >
-                <ListItemIcon>
-                    <DraftsIcon />
-                </ListItemIcon>
-                <ListItemText primary="Group2" />
-                </ListItem>
-            </List>
-        </div>
-
-    );
+  return (
+    <List className={classes.root}>
+      <ListItem onClick={test}>
+        <ListItemAvatar>
+          <Avatar>
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary="Denizcan Demir" secondary="Online" />
+      </ListItem>    
+    </List>
+  );
+}
 
 
-
+function test(){
+    alert("test");
 }
--- client/src/component/MessagePanel.js
+++ client/src/component/MessagePanel.js
@@ -4,6 +4,13 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Chip from '@material-ui/core/Chip';
 import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
+import SendIcon from '@material-ui/icons/Send';
+ 
+const socket = new WebSocket("ws://192.168.1.8:9090/socket/ws");
+
+
+
 
 export default function MessagePanel(){
 
@@ -19,18 +26,24 @@ export default function MessagePanel(){
       ]);
     };
 
+    const useStyles = makeStyles(theme => ({
+        button: {
+          margin: theme.spacing(1),
+        },
+      
+      }));
+      
 
 
 
 useEffect(() => {
-    const socket = new WebSocket("ws://192.168.1.8:9090/socket/ws");
-    
+
     const sMessage = document.getElementById("message");
     const sendButton = document.getElementById("send");
- 
-    
+
     socket.binaryType = "arraybuffer";
- 
+    
+
     socket.onopen = function (event) {
         console.log("Connected");
     };
@@ -47,6 +60,7 @@ useEffect(() => {
         msg.message=sMessage.value;
         msg.username=localStorage.getItem("username");
         sendMessage(JSON.stringify(msg));
+        sMessage.value="";
     };
  
     function sendMessage(msg) {
@@ -59,26 +73,30 @@ useEffect(() => {
 
 });
 
+    const classes = useStyles();
 
     return(
-        <div>
+        <Box>
             <Box height={window.innerHeight-75} width="100%">
-            <Box bgcolor="grey.300" height="90%" width="100%" display="inline-block">
-                <div id="messages">
-                <ul>
-                    {message.map(item => (
-                    <p><Chip avatar={<Avatar>M</Avatar>} size="small" label={item.message} color="secondary"/></p>
-                    ))}
-                </ul> 
-                </div>
+            <Box bgcolor="grey.300" height="90%" width="100%" display="inline-block" style={{overflow: 'auto'}}>
+                {message.map((item,i) => (
+                    <li key={i} style={{ listStyleType: "none", marginTop:"10px", marginLeft:"10px" }}>
+                        <Chip avatar={<Avatar></Avatar>} label={item.username +" : "+ item.message} size="medium" color="secondary"/>
+                    </li>   
+                ))}
             </Box>
-            <Box bgcolor="grey.500" height="35px" width="100%" display="inline-block">
-                <p>Type a message and hit send:</p>
-                <TextField width="21px" id="message"/>
-                <Button id="send" />
+            <Box  height="auto" width="100%" display="inline-block" >
+                <Box width="85%" display="inline-block">
+                    <TextField id="message" fullWidth placeholder="send message"/>
+                </Box>
+                <Box width="10%" display="inline-block">
+                    <Button id="send" fullWidth variant="contained" color="primary" className={classes.button} endIcon={<SendIcon></SendIcon>}>Send</Button>
+                </Box>
+                
+                
             </Box>
             </Box>
-        </div>
+        </Box>
 
     );
 
